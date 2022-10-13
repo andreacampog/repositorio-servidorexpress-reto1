5025d6e6c3128836b48dd2678c046d17b6e2b9ed
AndreaCampo <andreacampog@gmail.com>
2022-10-13 13:03:34 -0500

    primer envio

---
+++ modelos/Persona.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose');
+
+let PersonaSchema = new mongoose.Schema({
+    id: Number,
+    tipo_documento: String,
+    documento: String,
+    nombres: String,
+    apellidos:String,
+    direccion: String,
+    correo: String,
+    telefono: String,
+    celular: String,
+    link: String,
+    codigo_icfes: String,
+    familiar_asociado: String,
+    estrato_social: String,
+    tipo_colegio: String
+})
+
+module.exports= mongoose.model('persona', PersonaSchema, 'Personas');
